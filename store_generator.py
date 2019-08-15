2a966cb3941b913e4c0dbf43f8191572c45ded9d
NekroDarkmoon <junaid_maclaren97@hotmail.com>
2019-08-15 02:18:06 -0700

    Added Tavern Name generation

--- store_generator.py
+++ store_generator.py
@@ -9,7 +9,7 @@ import random
 
 # Store generator
 # Varaibles
-storeTypes = ['General Store']
+storeTypes = ['General Store', 'Tavern']
 selectedStore = []
 generatedStore = []
 
@@ -51,6 +51,7 @@ def generateStore(selectedStore):
     # Send data back to main to be printed
     return None
 
+
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #
@@ -59,13 +60,89 @@ def generateStore(selectedStore):
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-# TODO: Tavern Name
-# TODO: Tavern's Tale
-# TODO: The bartender
-# TODO: Clientle
-# TODO: Accomodations
-# TODO: Food
-# TODO: Drinks
+def tavern_generator():
+    # TODO: Tavern Name
+    name_prefix = ['striped', 'irritate', 'instinctive', 'zealous', 'sleet',
+                   'throat', 'death', 'fine', 'bouncy', 'sugar', 'slow', 'grade', 'adhesive',
+                   'match', 'bath', 'racial', 'pigs', 'alluring', 'debt', 'well-made',
+                   'superficial', 'snatch', 'debonair', 'annoyed', 'earn', 'notice', 'attract',
+                   'arch', 'soda', 'battle', 'panoramic', 'destruction', 'scintillating',
+                   'rainstorm', 'pen', 'dirt', 'puzzling', 'spade', 'boundary', 'frequent',
+                   'ethereal', 'crime', 'ill', 'trip', 'turn', 'foregoing', 'tricky', 'young',
+                   'knot', 'cumbersome', 'trust', 'vigorous', 'frame', 'houses', 'yummy',
+                   'steep', 'deserve', 'suggest', 'face', 'discovery', 'disastrous',
+                   'advertisement', 'terrify', 'rebel', 'sneaky', 'heavy', 'appreciate',
+                   'billowy', 'argument', 'queen', 'three', 'historical', 'system', 'wobble',
+                   'assorted', 'pancake', 'ice', 'week', 'dull', 'happy', 'beef', 'secret',
+                   'remain', 'plant', 'acoustic', 'rightful', 'yielding', 'kettle', 'fact',
+                   'nondescript', 'half', 'camp', 'tangible', 'agree', 'troubled', 'hang',
+                   'magic', 'regret', 'teeth', 'square', 'resonant', 'bloody', 'doctor',
+                   'thinkable', 'venomous', 'park', 'connect', 'amount', 'porter', 'angle',
+                   'flock', 'wanting', 'concerned', 'concentrate', 'floor', 'tickle',
+                   'defeated', 'word', 'wash', 'harmonious', 'abaft', 'sudden', 'creature',
+                   'graceful', 'smell', 'nostalgic', 'hope', 'share', 'disillusioned', 'cough',
+                   'poison', 'scrub', 'truck', 'different', 'tender', 'trick', 'minute',
+                   'noiseless', 'tacit', 'card', 'rhetorical', 'roof', 'letters', 'windy',
+                   'rejoice', 'prick', 'promise', 'force', 'uncle', 'wild', 'plant', 'belief',
+                   'complete', 'profuse', 'kiss', 'valuable', 'rat', 'soup', 'addition',
+                   'cloth', 'unsightly', 'balance', 'sister', 'ball', 'coast', 'shock', 'fat',
+                   'include', 'tenuous', 'interfere', 'start', 'unwritten', 'parallel',
+                   'stimulating', 'farm', 'expansion', 'calculate', 'point', 'spiky',
+                   'unbecoming', 'zephyr', 'useful', 'humdrum', 'heap', 'ubiquitous', 'curve',
+                   'groovy', 'bright', 'quizzical', 'wide-eyed', 'land', 'twist',
+                   'dysfunctional', 'birds', 'colorful', 'weak', 'upbeat', 'equable', 'reduce',
+                   'plants', 'plan', 'meek', 'limit', 'baseball', 'few', 'plastic', 'careless',
+                   'food', 'introduce', 'dizzy', 'magical', 'mice', 'scarce', 'stitch',
+                   'grandmother', 'confuse', 'phone', 'ludicrous', 'arrest', 'coach', 'damp',
+                   'toy', 'aware', 'amuse', 'sneeze', 'page', 'squeeze', 'rain', 'double',
+                   'superb', 'educated', 'songs', 'dance', 'electric', 'murky', 'silver',
+                   'play', 'leg', 'advise', 'sail', 'adamant', 'lip', 'useless', 'wail',
+                   'axiomatic', 'maddening', 'fireman', 'seal', 'drop', 'stereotyped', 'step',
+                   'slimy', 'dime', 'salt', 'mighty', 'vagabond', 'fortunate', 'mysterious',
+                   'rhythm', 'swanky', 'barbarous', 'great', 'grubby', 'peel', 'ripe', 'best',
+                   'quilt', 'note', 'burn', 'flat', 'table', 'wiggly', 'furniture', 'dinner',
+                   'simple', 'theory', 'flowery', 'wry', 'cows', 'mouth', 'broken', 'street',
+                   'plough', 'hate', 'threatening', 'hesitant', 'rifle', 'toothsome',
+                   'bedroom', 'pricey', 'insurance', 'frail', 'teeny-tiny', 'recognise',
+                   'deliver', 'tremble', 'loose', 'blue-eyed', 'perform', 'badge', 'six',
+                   'fearful', 'calendar', 'amusing', 'substantial', 'damage', 'scandalous',
+                   'cave', 'unpack', 'account', 'order', 'fang', 'tense', 'afraid', 'repeat',
+                   'flow', 'overwrought', 'bizarre', 'preserve', 'cannon', 'placid', 'meddle',
+                   'noisy', 'form', 'versed', 'weather', 'longing', 'fish', 'tasteful',
+                   'destroy', 'coal', 'sail', 'highfalutin', 'ban', 'cent', 'plain', 'lean',
+                   'vivacious', 'use', 'tiger', 'oatmeal', 'cut', 'fool', 'thing',
+                   'connection', 'form', 'fence', 'greasy']
+
+    name_suffix = ['knee', 'airport', 'throne', 'trick', 'sugar', 'swing',
+                   'apparatus', 'beds', 'tongue', 'anger', 'balance', 'arch',
+                   'wealth', 'flavor', 'gun', 'wheel', 'silver', 'wool', 'jar',
+                   'book', 'plastic', 'grandfather', 'house', 'rain', 'purpose'
+                   , 'year', 'letter', 'account', 'bushes',
+                   'use', 'egg', 'son', 'pets', 'wrench', 'mind', 'mom', 'pen',
+                   'hand', 'stranger',
+                   'crowd', 'copper', 'bucket', 'shock', 'rate', 'rat', 'amount',
+                   'station', 'condition', 'trail', 'border', 'distribution',
+                   'arm', 'protest', 'song', 'title', 'mass', 'shade',
+                   'baseball', 'animal', 'income', 'spring',
+                   'eggnog', 'government', 'behavior', 'canvas', 'dust',
+                   'error', 'substance', 'flag', 'bait', 'cave', 'detail',
+                   'representative',
+                   'legs', 'grape', 'guitar', 'produce', 'education', 'tub', 'blow',
+                   'grandmother', 'mouth', 'bath', 'mountain', 'person', 'look', 'car',
+                   'cherry', 'slave', 'slip', 'box', 'cakes', 'haircut', 'writing',
+                   'spark', 'building', 'noise', 'bed', 'sister', 'stomach']
+
+    tavern_name = 'The '
+    tavern_name += random.choice(name_prefix)
+    tavern_name += ' ' + random.choice(name_suffix)
+    print(tavern_name)
+
+    # TODO: Tavern's Tale
+    # TODO: The bartender
+    # TODO: Clientle
+    # TODO: Accomodations
+    # TODO: Food
+    # TODO: Drinks
 
 
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -89,5 +166,6 @@ def main():
     selectedStore.append(str(
         input('How many items do you wish for the store to have: ')))
 
-    generateStore(selectedStore)
+    # generateStore(selectedStore)
+    tavern_generator()
     print(generatedStore)
